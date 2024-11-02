fd84141c9ef2e057457043c18af9b24458dc0802
Mike Treit <mtreit@ntdev.microsoft.com>
2024-11-01 18:37:37 -0700

    Small cleanup.

--- publishDocker.ps1
+++ publishDocker.ps1
@@ -1,5 +1,5 @@
 $acr = "stockrippercr"
-az acr login --name stockrippercr
+az acr login --name $acr
 docker build -t stockripper:latest .
 docker tag stockripper:latest $("$acr.azurecr.io/stockripper:latest")
 docker push $("$acr.azurecr.io/stockripper:latest")
