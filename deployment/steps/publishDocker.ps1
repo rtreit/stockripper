0c7c8417c319a481a1062b6147ce4411ae7d2603
Randy Treit <randyt@outlook.com>
2024-11-01 22:24:00 -0700

    Test calling Python container from F# container

--- deployment/steps/publishDocker.ps1
+++
@@ -1,5 +0,0 @@
-$acr = "stockrippercr"
-az acr login --name stockrippercr
-docker build -t stockripper:latest .
-docker tag stockripper:latest $("$acr.azurecr.io/stockripper:latest")
-docker push $("$acr.azurecr.io/stockripper:latest")
--- src/fsharp-app/Program.fs
+++ src/fsharp-app/Program.fs
@@ -1,2 +1,28 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.Net.Http
+open System.Threading.Tasks
+
+let httpClient = new HttpClient()
+
+let getHealthCheckAsync() =
+    task {
+        let url = "http://stockripper-python-app.stockripper.internal:5000/health"
+        printfn "Sending request to Python container at %s" url
+        try
+            let! response = httpClient.GetAsync(url)
+            if response.IsSuccessStatusCode then
+                let! content = response.Content.ReadAsStringAsync()
+                printfn "Response from Python container: %s" content
+            else
+                printfn "Failed to reach Python container. Status code: %d" (int response.StatusCode)
+        with
+        | ex -> printfn "Error calling Python container: %s" ex.Message
+    }
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello from F#"
+
+    // Call the health check endpoint and print results
+    getHealthCheckAsync().Wait()
+    0 // return an integer exit code
