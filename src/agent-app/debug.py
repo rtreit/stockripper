c358a473ef87a7c7a3a9c7d39bd2e61e421c47ec
Mike Treit <mtreit@ntdev.microsoft.com>
2024-11-02 16:00:10 -0700

    Small cleanup.

--- src/agent-app/debug.py
+++ src/agent-app/debug.py
@@ -3,14 +3,12 @@ import requests
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+fsharpUri = "http://stockripper-fsharp-app.stockripper.internal:5001/health"
 
 @app.route('/health', methods=['GET'])
 def health_check():
-    response = requests.get("http://stockripper-fsharp-app.stockripper.internal:5001/health")
-    health_data = response.json()
-    health_check = health_data.get("status")
-    print(response)
-    print(health_data)
+    response = requests.get(fsharpUri)
+    health_check = response.json().get("status")
     return jsonify(status=f"I'm alive - response from calling FSharp service: {health_check}"), 200
 
 if __name__ == '__main__':
