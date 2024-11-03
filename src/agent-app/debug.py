a074b24a6975c0f7bed77fc47c928c42555e3bc4
Randy Treit <randyt@outlook.com>
2024-11-02 21:15:34 -0700

    Add a bit more console logging

--- src/agent-app/debug.py
+++ src/agent-app/debug.py
@@ -1,15 +1,20 @@
-# health_check.py
 import requests
 from flask import Flask, jsonify
+import logging
 
 app = Flask(__name__)
 fsharpUri = "http://stockripper-fsharp-app.stockripper.internal:5001/health"
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
 @app.route('/health', methods=['GET'])
 def health_check():
+    logging.info('Received a health check request')
     response = requests.get(fsharpUri)
     health_check = response.json().get("status")
+    logging.info(f'Response from FSharp service: {health_check}')
     return jsonify(status=f"I'm alive - response from calling FSharp service: {health_check}"), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Expose on all interfaces on port 5000
+    app.run(host='0.0.0.0', port=5000)
--- src/fsharp-app/Controllers/HealthController.fs
+++ src/fsharp-app/Controllers/HealthController.fs
@@ -5,6 +5,7 @@ open Microsoft.Extensions.Logging
 open System.Net.Http
 open System.Net.Http.Json
 open StockRipperFS
+
 type HealthCheckResponse = { Status: string }
 
 [<ApiController>]
@@ -13,13 +14,17 @@ type HealthController (logger : ILogger<HealthController>) =
     inherit ControllerBase()
     let client = new HttpClient()
     let agentUri = Utils.agentUri
+
     let PingAgentAsync =
         async {
             try
+                logger.LogInformation("Pinging agent service at {Uri}", agentUri)
                 let! response = client.GetFromJsonAsync<HealthCheckResponse>(agentUri) |> Async.AwaitTask
+                logger.LogInformation("Received response from agent service: {Status}", response.Status)
                 return response.Status
             with
             | ex ->
+                logger.LogError(ex, "An error occurred while pinging the agent service")
                 return $"Error occurred: {ex.Message}"
         }
 
@@ -27,13 +32,16 @@ type HealthController (logger : ILogger<HealthController>) =
     member _.Get() : Async<ActionResult> =
         let context = base.HttpContext
         async {
+            logger.LogInformation("Received health check request")
             match context.Request.Query with
             | query when query.ContainsKey("pingAgent") ->
                 let! response = PingAgentAsync
+                logger.LogInformation("Sending response: {Status}", response)
                 return OkObjectResult({ Status = $"Healthy - response from pinging agent service: '{response}'" })
             | query when query.ContainsKey("simulateError") ->
+                logger.LogWarning("Simulating error response")
                 return BadRequestResult()
             | _ ->
+                logger.LogInformation("Sending default healthy response")
                 return OkObjectResult({ Status = "Healthy" })
         }
-
