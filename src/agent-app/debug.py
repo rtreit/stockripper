b93a58a1c0ddc44bd70a4489ca80a8ec7a4b541a
Randy Treit <randyt@outlook.com>
2024-11-03 20:08:27 -0800

    Add rust container

--- src/agent-app/debug.py
+++ src/agent-app/debug.py
@@ -1,9 +1,11 @@
 import requests
 from flask import Flask, jsonify
 import logging
+import time
 
 app = Flask(__name__)
 fsharpUri = "http://stockripper-fsharp-app.stockripper.internal:5001/health"
+rustUri = "http://stockripper-rust-app.stockripper.internal:5002/health"
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -17,4 +19,15 @@ def health_check():
     return jsonify(status=f"I'm alive - response from calling FSharp service: {health_check}"), 200
 
 if __name__ == '__main__':
+    logging.info('Agent app started')
+    logging.info('Hello from the agent app running Python')
+    time.sleep(5)
+    try:
+        rust_response = requests.get(rustUri)
+        logging.info(f'Initial Response from Rust service: {rust_response.text}')
+        logging.info(f'Response from Rust service: {rust_response.json().get("status")}')
+    except Exception as e:
+        logging.error(f'Error when calling Rust service: {e}')
+    fsharp_response = requests.get(fsharpUri)
+    logging.info(f'Response from FSharp service: {fsharp_response.json().get("status")}')    
     app.run(host='0.0.0.0', port=5000)
