01b5632549c725634380470bf686e60677363836
Randy Treit <randyt@outlook.com>
2024-11-24 18:57:35 -0800

    Switch to NGINX

--- deployment/steps/createPublicIP.ps1
+++
@@ -1,8 +0,0 @@
-$resourceGroupName = "stockripper"
-$publicIpName = "stockRipperChatAppPublicIp"
-
-az network public-ip create `
-  --resource-group $resourceGroupName `
-  --name $publicIpName `
-  --allocation-method Static `
-  --sku Standard
